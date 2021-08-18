5d19ba0a6518a3e1e0bd2d6e7dde9accd275dcec
amder111 <abhirupmajumder2@gmail.com>
2021-08-18 16:31:42 +0530

    evenOdd

---
+++ evenORodd.cpp
@@ -0,0 +1,12 @@
+// https://prepinsta.com/cpp-program/cpp-program-to-check-whether-a-number-is-even-or-odd/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    if(n%2==0) cout << n <<" is even" ;
+    else cout << n << " is odd";
+}
