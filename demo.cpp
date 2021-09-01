90c71218f86f2568b6c624df689cae4f49953a02
amder111 <52421748+amder111@users.noreply.github.com>
2021-09-01 16:09:28 +0530

    fst code

---
+++ demo.cpp
@@ -0,0 +1,13 @@
+#include<iostream>  //pre-processor
+using namespace std;
+// iostream -> header file
+// main function
+int main(){
+    double p;
+    cin >> p;
+    cout << p;
+    cout << "\nI'm learning cpp";
+    // c=console
+    // out=output
+    // :: -> scope resolution
+}
